d39ea64c969b0a90ecd0d5bdf4cc46b6a63eef8d
Wolfgang Nagele <wn@exabit.at>
2011-12-02 21:27:59 +0100

    Remove unnecessary calls to get client Remove methods that are not implemented and implement seek method

--- src/main/java/org/apache/hadoop/fs/sftp/SFTPInputStream.java
+++ src/main/java/org/apache/hadoop/fs/sftp/SFTPInputStream.java
@@ -5,28 +5,31 @@ import java.io.IOException;
 import org.apache.hadoop.fs.FSInputStream;
 import org.apache.hadoop.fs.FileSystem;
 
+import ch.ethz.ssh2.SFTPv3Client;
 import ch.ethz.ssh2.SFTPv3FileHandle;
 
 public class SFTPInputStream extends FSInputStream {
 	private SFTPv3FileHandle handle;
 	private FileSystem.Statistics stats;
+	private SFTPv3Client client;
 	private long pos = 0;
 
 	public SFTPInputStream(SFTPv3FileHandle handle, FileSystem.Statistics stats) {
 		this.handle = handle;
 		this.stats = stats;
+		this.client = handle.getClient();
 	}
 
 	@Override
 	public void close() throws IOException {
 		super.close();
-		handle.getClient().closeFile(handle);
+		client.closeFile(handle);
 	}
 
 	@Override
 	public synchronized int read() throws IOException {
 		byte[] buf = new byte[1];
-        int read = handle.getClient().read(handle, pos, buf, 0, 1);
+        int read = client.read(handle, pos, buf, 0, 1);
         if (read == -1)
         	return -1;
 
@@ -43,7 +46,7 @@ public class SFTPInputStream extends FSInputStream {
 
 	@Override
 	public int read(long position, byte[] buf, int off, int len) throws IOException {
-        int read = handle.getClient().read(handle, position, buf, off, len);
+        int read = client.read(handle, position, buf, off, len);
         if (read == -1)
         	return -1;
 
@@ -59,21 +62,8 @@ public class SFTPInputStream extends FSInputStream {
 	}
 
 	@Override
-	public boolean markSupported() {
-		return false;
-	}
-
-	@Override
-	public void mark(int readLimit) {}
-
-	@Override
-	public void reset() throws IOException {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
 	public void seek(long targetPos) throws IOException {
-		throw new UnsupportedOperationException();
+		pos = targetPos;
 	}
 
 	@Override
